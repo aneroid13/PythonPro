eb0667f4af5a4f00c8e8129a1a93004c6a27218a
ant <aneroid@inbox.ru>
2023-12-03 15:03:56 +0700

    django fix flake

--- 07/questions/migrations/0004_customuser_alter_answer_user_alter_question_user_and_more.py
+++ 07/questions/migrations/0004_customuser_alter_answer_user_alter_question_user_and_more.py
@@ -18,7 +18,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CustomUser',
             fields=[
-                ('user_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('user_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE,
+                                                  parent_link=True, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
                 ('pict', models.ImageField(upload_to='', verbose_name='Your image')),
             ],
             options={
